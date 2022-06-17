871198f880c630b275990db412e0643939ebcdaf
Koi <koido.t.aa@m.titech.ac.jp>
2022-06-17 16:36:53 +0900

    Github使えるかのテスト
    ScriptにTest2_koiを追加しました

---
+++ Assets/Scripts/Test2_koi.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2_koi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //このscriptは消しても大丈夫です
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
