7b4406d48fc09b29cb9041b3f1ec59a0333c1f48
pikachu0310 <sunnon@i.softbank.jp>
2022-06-18 16:10:52 +0900

    アイテムとハードルが降るように、後いろいろ調整しました

--- Assets/Scripts/TateScroll.cs
+++ Assets/Scripts/TateScroll.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,3 +27,35 @@ public class TateScroll : MonoBehaviour
         }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TateScroll : MonoBehaviour
+{
+
+    // スクロールスピードをここで定義
+    [SerializeField] float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //下方向にスクロール
+        transform.position -= new Vector3(0, Time.deltaTime * speed);
+
+        //Yが-29まで来れば、61まで移動する
+        if (transform.position.y <= -48f)
+        {
+            float z = Mathf.FloorToInt(Random.Range(1, 1));
+            z *= 0.001f;
+            transform.position = new Vector3(0, 96f, 10+z);
+        }
+    }
+}
+>>>>>>> Stashed changes
